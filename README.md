2ef3be1a8841ae1e9a53676a0fe9a26865da275a
furikuda <19654092+Furikuda@users.noreply.github.com>
2018-09-19 22:46:01 +0200

    added recaptcha & stuffé

--- README.md
+++ README.md
@@ -3,3 +3,16 @@ Website for furry related stuff in Switzerland. It is actually hosted at:
 
 [furday.duckdns.org](https://furday.duckdns.org)
 [goto.ninja/furday](https://goto.ninja/furday)
+
+There is a stupid reCaptcha to try and fend off bots while keeping
+links to Telegram groups in the calendar.
+
+## Install
+
+```
+apt install ruby-sinatra ruby-slim
+cp config_template.json config.json
+# Add your reCaptcha sitekey and calendar IDs there
+
+ruby index.rb
+```
--- index.rb
+++ index.rb
@@ -3,21 +3,38 @@
 
 Encoding.default_external = Encoding::UTF_8
 
+require "json"
 require "sinatra"
 require "slim"
 require "slim/include"
 
-set :public_folder, File.join(File.dirname(__FILE__),'public')
-set :show_exceptions, false 
-use Rack::Session::Cookie,  :key => 'rack.session',
-                            :domain=>'furday.duckdns.org',
-                            :path=>'/',
-                            :expire_after=>86400*2,#Inseconds
-                            :secret=> SecureRandom.hex # Kills all sessions on restart, which is fine
+config_file = "config.json"
 
+begin
+    $config = JSON.parse(File.read(config_file))
+rescue Exception => e
+    $stderr.puts "Please rename config_template.json to #{config_file} and edit it"
+    raise e
+end
+
+set :public_folder, File.join(File.dirname(__FILE__),'public')
+set :show_exceptions, false
+use Rack::Session::Cookie,  :key => 'rack.session',
+                            :domain => $config['domain'],
+                            :path => '/',
+                            :expire_after => 86400*2,#Inseconds
+                            :secret => SecureRandom.hex # Kills all sessions on restart, which is fine
 
 get '/' do
+    @sitekey = $config['sitekey']
     slim :main
+end
+
+get '/agenda' do
+    @agenda_url = $config.dig("agendas").map{ |a|
+        "src=#{a['agenda_id']}%40group.calendar.google.com&color=%23#{a['color']}"
+    }.join('&')
+    slim :agenda
 end
 
 get '/links' do
